8046169e3c1b3c18bac33e045883d46d7ea258e8
matteo.cavallo <matteo.cavallo@flosslab.com>
2024-11-23 11:50:58 +0100

    remove base from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/claudia-site/',
-  resolve: {
-    alias: {
-      '@assets': `/claudia-site/assets`,
-    },
-  },
-  server: {
-    middlewareMode: false,
-  },
-})
+  // base: "/claudia-site/",
+  // build: {
+  //   outDir: "dist/claudia-site", // Specifica la sottocartella di output
+  //   // rollupOptions: {
+  //   //   output: {
+  //   //     // Mantieni i percorsi relativi per gli asset
+  //   //     assetFileNames: "assets/[name]-[hash][extname]",
+  //   //     chunkFileNames: "assets/[name]-[hash].js",
+  //   //     entryFileNames: "assets/[name]-[hash].js",
+  //   //   },
+  //   // },
+  // },
+
+  // resolve: {
+  //   alias: {
+  //     '@assets': `/claudia-site/assets`,
+  //   },
+  // },
+  // server: {
+  //   middlewareMode: false,
+  // },
+});
