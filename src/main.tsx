04c7498cace164e16af70d9cab68146b57d30b39
matteo.cavallo <matteo.cavallo@flosslab.com>
2024-11-25 17:30:52 +0100

    add hash to route

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./fonts.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
 import { Worker } from "@react-pdf-viewer/core";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
         <App />
       </Worker>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
